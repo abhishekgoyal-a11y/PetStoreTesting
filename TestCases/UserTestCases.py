e16d8c71549bac3af5312b3ba10291d5b7fff5e9
Abhishek Goyal <58354473+abhishekgoyal-a11y@users.noreply.github.com>
2024-01-05 19:56:55 +0530

    create_user_test test case and data added

--- TestCases/UserTestCases.py
+++ TestCases/UserTestCases.py
@@ -3,4 +3,4 @@ from EndPoints.UserEndPoints import UserEndPoints
 def create_user_test(url, user_id, username, firstName, lastName, email, password, phone, userStatus, expected_status_code):
     user = UserEndPoints(url)
     response = user.create_user(user_id, username, firstName, lastName, email, password, phone, userStatus)
-    assert response.status_code==expected_status_code
+    assert response.status_code==expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}. Error message: {response.text}"
