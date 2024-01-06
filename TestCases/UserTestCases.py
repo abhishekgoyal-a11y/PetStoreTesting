24f160d193344c723668797a8baa6e9501a4b28d
Abhishek Goyal <58354473+abhishekgoyal-a11y@users.noreply.github.com>
2024-01-06 13:42:48 +0530

    case and data added

--- TestCases/UserTestCases.py
+++ TestCases/UserTestCases.py
@@ -4,3 +4,23 @@ def create_user_test(url, user_id, username, firstName, lastName, email, passwor
     user = UserEndPoints(url)
     response = user.create_user(user_id, username, firstName, lastName, email, password, phone, userStatus)
     assert response.status_code==expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}. Error message: {response.text}"
+
+def user_login_test(url, username, password, expected_status_code):
+    user = UserEndPoints(url)
+    response = user.user_login(username, password)
+    assert response.status_code==expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}. Error message: {response.text}"
+
+def get_user_test(url, username, expected_status_code):
+    user = UserEndPoints(url)
+    response = user.get_user(username)
+    assert response.status_code==expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}. Error message: {response.text}"
+
+def update_user_test(url, user_id, username, newUsername, firstName, lastName, email, password, phone, userStatus, expected_status_code):
+    user = UserEndPoints(url)
+    response = user.update_user(user_id, username, newUsername, firstName, lastName, email, password, phone, userStatus)
+    assert response.status_code==expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}. Error message: {response.text}"
+
+def delete_user_test(url, username, expected_status_code):
+    user = UserEndPoints(url)
+    response = user.delete_user(username)
+    assert response.status_code==expected_status_code, f"Expected status code {expected_status_code}, but got {response.status_code}. Error message: {response.text}"
--- TestExecution.py
+++ TestExecution.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 from Config import DefaultsConfig
-from TestCases.UserTestCases import create_user_test
+from TestCases.UserTestCases import *
 from TestDataManagement import TestDataManagement
 
 def setup_module():
@@ -10,8 +10,17 @@ def setup_module():
     defaults_config.read_yaml_file(os.getcwd()+'\\Config.yaml')
     url = defaults_config.get_config_value("url")
 
-@pytest.mark.parametrize("action, user_id, username, firstName, lastName, email, password, phone, userStatus, expected_status_code",
+@pytest.mark.parametrize("action, user_id, username, newUsername, firstName, lastName, email, password, phone, userStatus, expected_status_code",
                          TestDataManagement().read_test_data("Users").values)
-def test_execute(action, user_id, username, firstName, lastName, email, password, phone, userStatus, expected_status_code):
+def test_execute(action, user_id, username, newUsername, firstName, lastName, email, password, phone, userStatus, expected_status_code):
+
     if action == "create_user_test":
         create_user_test(url, user_id, username, firstName, lastName, email, password, phone, userStatus, expected_status_code)
+    elif action == "user_login_test":
+        user_login_test(url, username, password, expected_status_code)
+    elif action == "get_user_test":
+        get_user_test(url, username, expected_status_code)
+    elif action == "delete_user_test":
+        delete_user_test(url, username, expected_status_code)
+    elif action == "update_user_test":
+        update_user_test(url, user_id, username, newUsername, firstName, lastName, email, password, phone, userStatus, expected_status_code)
