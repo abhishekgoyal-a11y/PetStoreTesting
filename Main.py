e611359da338294261af6ed05ebbad421c0d5997
Abhishek Goyal <58354473+abhishekgoyal-a11y@users.noreply.github.com>
2024-01-09 13:46:21 +0530

    code updated

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 from datetime import datetime
 
+if not os.path.exists('Reports'):
+    os.makedirs('Reports')
 script_path = os.path.abspath('./TestExecution.py')
 report_path = os.path.abspath(f'./Reports/report{datetime.now().strftime("%Y%m%d%H%M%S")}.html')
 command = f'pytest {script_path} --html={report_path}'
