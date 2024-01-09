059b5b9e79d75cc505cc331268206c79f77ab671
Abhishek Goyal <58354473+abhishekgoyal-a11y@users.noreply.github.com>
2024-01-09 12:17:24 +0530

    code added

--- main.py
+++
@@ -1,7 +0,0 @@
-from EndPoints.UserEndPoints import UserEndPoints
-
-user = UserEndPoints("https://petstore.swagger.io/v2/")
-user.create_user(
-    2,
-                 'username', 'firstName', 'lastName', 'email', 'password',
-                 'phone', 1)
